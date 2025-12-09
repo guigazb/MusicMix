e8f3415ca8a6912b4a79347e55cb2d6c26082e71
Beneilton Martins <contato.beneilton.martins@gmail.com>
2025-12-09 11:56:53 -0300

    wip-v2.0.2-push-post

--- frontend/src/js/crud/create.js
+++ frontend/src/js/crud/create.js
@@ -1,12 +1,14 @@
 import { createArtist } from '../api/artistsApi.js';
-import { allArtists } from '../data/artists.js';
+import { allArtists, setAllArtists } from '../data/artists.js';
 import { showToast } from '../ui/toast.js';
 import { renderGrid } from '../ui/render.js';
 
 export async function handleCreate(data) {
   try {
     const novo = await createArtist(data);
-    allArtists.push(novo);
+    const updated = [...allArtists, novo]; // Adiciona o novo artista
+    setAllArtists(updated); // Atualiza a memória
+    //allArtists.push(novo);
     showToast('Artista adicionado!');
     renderGrid();
   } catch (err) {
--- frontend/src/js/crud/update.js
+++ frontend/src/js/crud/update.js
@@ -1,5 +1,5 @@
 import { updateArtist } from '../api/artistsApi.js';
-import { allArtists } from '../data/artists.js';
+import { allArtists, setAllArtists } from '../data/artists.js';
 import { showToast } from '../ui/toast.js';
 import { renderGrid } from '../ui/render.js';
 
@@ -7,7 +7,11 @@ export async function handleUpdate(id, data) {
   try {
     const updated = await updateArtist(id, data);
     const index = allArtists.findIndex(a => a.id === id);
-    allArtists[index] = updated;
+    // Cria uma nova array com o artista atualizado
+    const newArtists = [...allArtists];
+    newArtists[index] = updated;
+    setAllArtists(newArtists);
+    //allArtists[index] = updated;
     showToast('Artista atualizado!');
     renderGrid();
   } catch (err) {
